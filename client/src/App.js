d76eee0c56571569f65ee591dcb339d8950aa1d7
䂊汐 <28.juxi.lu@gmail.com>
2019-05-02 00:08:20 -0400

    shows leaflet map on page with pin

---
+++ client/src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import L from 'leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
+import './App.css';
+
+var myIcon = L.icon({
+    iconUrl: 'my-icon.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowUrl: 'my-icon-shadow.png',
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
+
+class App extends Component {
+  state = {
+    location: {
+      lat: 51.505,
+      lng: -0.09,
+    },
+    haveUsersLocation: false,
+    zoom: 2,
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        location: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        },
+        haveUsersLocation: true,
+        zoom: 13;
+      });
+    }, () => {
+      console.log("oops no location given");
+      fetch('https://ipapi.co/json')
+        .then(res => res.json())
+        .then(location => {
+          console.log(location);
+          this.setState({
+            location: {
+              lat: location.latitude,
+              lng: location.longitude
+            },
+            haveUsersLocation: true,
+            zoom: 13;
+          });
+        })
+    });
+  }
+
+  render() {
+    const position = [this.state.location.lat, this.state.location.lng];
+    return (
+      <div className="map">
+        <Map className="map" center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          { this.statehaveUsersLocation ?
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker> : ''
+          }
+        </Map>
+        <Card body className="message-form">
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Go somewhere</Button>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default App;
